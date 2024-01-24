1b7e42c743df8d5a8607df74b0bef5852feb523c
GliderTech <71361118+michaelglider@users.noreply.github.com>
2024-01-24 11:50:23 -0800

    Add files via upload

---
+++ 853-car-fleet.py
@@ -0,0 +1,34 @@
+#853. Car fleet problem description: 
+#There are n cars going to the same destination along a one-lane road. The destination is target miles away.
+#You are given two integer array position and speed, both of length n, where position[i] is the position of 
+#the ith car and speed[i] is the speed of the ith car(in miles per hour.)
+#A car can never pass anothyer car ahead of it, but it can catch up to it and drive bumper to bumper at the same speed. 
+#The faster car will slow down to match the slower car's speed. The distance between these two cars is ignored (i.e., they are 
+#assumed to have the same position).
+#A car fleet is some non-empty set of cars driving at the same position and same speed. Note that a single car is also a car fleet.
+#If a car catches up to a car fleet right at the destination point, it will still be considered as one car fleet.
+#Return the number of car fleets that will arrive at the destination.
+
+class Solution(object):
+    def carFleet(self, target, pos, speed):
+
+        #Calculate the time each car needs to reach the target and store it in a list
+        time = [float(target - p) / s for p, s in sorted(zip(pos, speed))]
+
+        #initialize result and current variables
+        res = cur = 0
+
+        #loop over the time list in reverse order
+        for t in time[::-1]:
+
+            #if t is greater than the max current time
+            if t > cur:
+
+                #add one to the result
+                res += 1
+
+                #set the current equal to the time
+                cur = t
+
+        #return the result variable as it stores the amount of car fleets
+        return res
